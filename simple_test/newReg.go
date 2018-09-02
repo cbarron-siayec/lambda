3c96866afb10b7d975affa263248f67bab824521
cbarron-siayec <38666243+cbarron-siayec@users.noreply.github.com>
2018-09-02 16:56:52 -0500

    ok

--- simple_test/newReg.go
+++ simple_test/newReg.go
@@ -58,7 +58,7 @@ func handler(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse,
 	}
 	return events.APIGatewayProxyResponse{
 		StatusCode: 201,
-		Headers:    map[string]string{"Registro": fmt.Sprintln(req), "Contentttttt-Type": "application/json", "Access-Control-Allow-Origin": "*"},
+		Headers:    map[string]string{"Registro": fmt.Sprintln(req), "Content-Type": "application/json", "Access-Control-Allow-Origin": "*"},
 	}, nil
 }
 

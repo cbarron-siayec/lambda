abab0a35939e02e067be1f735615179368bbd157
cbarron-siayec <38666243+cbarron-siayec@users.noreply.github.com>
2018-09-07 17:14:53 -0500

    Funcionando

--- consultores_funcionales/js/check-auth.js
+++ consultores_funcionales/js/check-auth.js
@@ -1,15 +1,11 @@
+//var cognitoUser = new AWSCognito.CognitoIdentityServiceProvider.CognitoUser({
+//          Username : username,
+//          Pool : userPool
+//});
   var cognitoUser = userPool.getCurrentUser();
-  var windowCheck;
-  cognitoUser.getSession(function sessionCallback(err, session) {
-                  if (err) {
-                      windowCheck = "";
-                  } else if (!session.isValid()) {
-                      windowCheck = "";
-                  } else {
-                      windowCheck = session.getIdToken().getJwtToken();
-                  }
-  });
-if (windowCheck == "") {
-  window.location.href = "../index.html";
-  logout();
-}
+  if(cognitoUser){
+  }
+  else {
+    window.location.href = "../index.html";
+    logout();
+  }

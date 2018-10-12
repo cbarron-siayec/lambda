982c468972af578b2b3163f55ef2f75d809c4cd2
cbarron-siayec <38666243+cbarron-siayec@users.noreply.github.com>
2018-10-12 13:30:12 -0500

    s

--- drp/drp.go
+++ drp/drp.go
@@ -1,29 +1,14 @@
 package main
 
 import (
-	"context"
 	"log"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func handler(ctx context.Context, kinesisEvent events.KinesisEvent) error {
+func handler(kinesisEvent events.KinesisEventRecord) error {
 	log.Print(kinesisEvent)
-	log.Print(kinesisEvent.Records[0].Kinesis.ApproximateArrivalTimestamp)
-	log.Print(kinesisEvent.Records[0].Kinesis.EncryptionType)
-	log.Print(kinesisEvent.Records[0].Kinesis.KinesisSchemaVersion)
-	log.Print(kinesisEvent.Records[0].Kinesis.PartitionKey)
-	log.Print(kinesisEvent.Records[0].Kinesis.SequenceNumber)
-	log.Print(kinesisEvent.Records[0].Kinesis.Data)
-	for _, record := range kinesisEvent.Records {
-		kinesisRecord := record.Kinesis
-		dataBytes := kinesisRecord.Data
-		dataText := string(dataBytes)
-		log.Print(record.Kinesis.Data)
-		log.Print(dataText)
-
-	}
 	return nil
 }
 
